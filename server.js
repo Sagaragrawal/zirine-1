12ddadf5ca866510cd21e5e0764be5293bfa6ccd
sunnykarira <sunnykarira@outlook.com>
2016-02-07 10:36:45 +0530

    Basic front page design

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var ejs = require('ejs');
+var engine = require('ejs-mate');
+var PORT = 3000 || process.env.PORT;
+
+
+var app = express();
+//app.use(bodyParser());
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+// Engine
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+
+
+app.get('/', function (req, res){
+
+	res.render('main/home_front');
+});
+
+
+app.listen(PORT, function (err){
+
+	if(err) throw err;
+	console.log('App listening at port ' + PORT);
+})
